f4f11aba5726d611d39679729d2da782472337bb
Abrahamcoeurdepierre <letmeloveyounow27@gmail.com>
2020-09-09 21:34:31 -0700

    Did the button and add my name

--- app/src/main/java/com/example/group7/MainActivity.java
+++ app/src/main/java/com/example/group7/MainActivity.java
@@ -2,7 +2,10 @@ package com.example.group7;
 
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.content.Intent;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
 
 public class MainActivity extends AppCompatActivity {
 
@@ -10,5 +13,17 @@ public class MainActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+       Button btn = findViewById(R.id.next);
+       btn.setOnClickListener(new View.OnClickListener() {
+           @Override
+           public void onClick(View view) {
+               openActivity2();
+           }
+       });
+
+    }
+    public void openActivity2(){
+        Intent intent = new Intent(this, Activity2.class);
+        startActivity(intent);
     }
 }
