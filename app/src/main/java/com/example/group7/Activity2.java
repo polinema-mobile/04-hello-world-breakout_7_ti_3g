12519afae471a40441b770ac954974d85e9299bc
Silami-tech <yuk63442@gmail.com>
2020-09-10 12:54:41 +0700

    I added my name and the previous button

--- app/src/main/java/com/example/group7/Activity2.java
+++ app/src/main/java/com/example/group7/Activity2.java
@@ -1,14 +1,27 @@
 package com.example.group7;
 
-import androidx.appcompat.app.AppCompatActivity;
-
+import android.content.Intent;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
 
-public class Activity2 extends AppCompatActivity {
+public class Activity2 extends MainActivity {
+
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_2);
+        Button btn = findViewById(R.id.previous);
+        btn.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                openActivity2();
+            }
+        });
+    }
+    public void openActivity2(){
+        Intent intent = new Intent(this, MainActivity.class);
+        startActivity(intent);
     }
 }
